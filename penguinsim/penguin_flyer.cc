5b79978e45d9af0f0bce19a06bd551dc0595d740
mark-todd <markpeter.todd@hotmail.co.uk>
2022-05-04 09:34:27 +0000

    Added fish catcher class

--- penguinsim/penguin_flyer.cc
+++ penguinsim/penguin_flyer.cc
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
+#include <list>
+#include <map>
+#include <stdexcept>
+#include <cassert>
+#include <random>
+#include <iterator>
 
 class Tracker {
     private:
@@ -64,6 +70,134 @@ class Penguin {
 
 };
 
+std::list<std::string> split_string (std::string input_string, std::string delimiter) {
+    size_t pos = 0;
+    std::string token;
+    std::list<std::string> output_list;
+    while (true) {
+        pos = input_string.find(delimiter);
+        if (pos != std::string::npos) {
+            token = input_string.substr(0, pos);
+            output_list.push_back(token);
+            input_string.erase(0, pos + delimiter.length());
+        }
+        else {
+            token = input_string.substr(0, pos);
+            output_list.push_back(token);
+            break;
+        }
+    }
+    return output_list;
+}
+
+namespace RandomUtils {
+    template<typename Iter, typename RandomGenerator>
+    Iter select_randomly(Iter start, Iter end, RandomGenerator& g) {
+        std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
+        std::advance(start, dis(g));
+        return start;
+    }
+
+    template<typename Iter>
+    Iter select_randomly(Iter start, Iter end) {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        return select_randomly(start, end, gen);
+    }
+}
+
+namespace Fishing {
+    enum FishingRanges {short_range = 0, medium_range = 1, long_range = 2};
+
+    FishingRanges str_to_fish_range(std::string input_string) {
+        if (input_string == "short") {
+            return short_range;
+        }
+        else if (input_string == "medium") {
+            return medium_range;
+        }
+        else if (input_string == "long") {
+            return long_range;
+        }
+        else {
+            throw std::invalid_argument("invalid string");
+        }
+    }
+
+    class Fish {
+        public:
+            std::string name;
+            FishingRanges range;
+            double calorific_value;
+
+            Fish(
+                std::string name,
+                FishingRanges range,
+                double calorific_value
+            ) : name(name), range(range), calorific_value(calorific_value) {}
+    };
+
+    class FishCatcher {
+        private:
+            std::string delimiter;
+            std::ifstream fish_file_stream;
+            std::list<Fish> sr_fish_catalogue;
+            std::list<Fish> mr_fish_catalogue;
+            std::list<Fish> lr_fish_catalogue;
+        public:
+            FishCatcher(
+                std::string fish_file
+            ) {
+                fish_file_stream.open(fish_file);
+                std::string line;
+                while ( std::getline(fish_file_stream, line) ) {
+                    std::list<std::string> line_list;
+                    FishingRanges associated_range;
+                    line_list = split_string(line, ",");
+                    std::string range;
+                    std::string fish_name;
+                    std::string fish_cals;
+                    if (line_list.size() == 3) {
+                        range = line_list.front();
+                        line_list.pop_front();
+                        fish_name = line_list.front();
+                        line_list.pop_front();
+                        fish_cals = line_list.front();
+                        line_list.pop_front();
+                    }
+                    else {
+                        throw std::invalid_argument("Size not 3"); 
+                    }
+                    assert(line_list.size() == 0);
+                    associated_range = str_to_fish_range(range);
+                    Fish new_fish(fish_name, associated_range, std::stod(fish_cals));
+                    if (associated_range == short_range) {
+                        sr_fish_catalogue.push_back(new_fish);
+                    }
+                    else if (associated_range == medium_range) {
+                        mr_fish_catalogue.push_back(new_fish);
+                    }
+                    else {
+                        lr_fish_catalogue.push_back(new_fish);
+                    }
+                    
+                }
+            }
+            Fish catch_fish(FishingRanges range) {
+                if (range == short_range) {
+                    return *RandomUtils::select_randomly(sr_fish_catalogue.begin(), sr_fish_catalogue.end());
+                }
+                else if (range == medium_range) {
+                    return *RandomUtils::select_randomly(mr_fish_catalogue.begin(), mr_fish_catalogue.end());
+                }
+                else {
+                    return *RandomUtils::select_randomly(lr_fish_catalogue.begin(), lr_fish_catalogue.end());
+                }
+            };
+
+    };
+}
+
 
 int main()
 {
@@ -71,6 +205,9 @@ int main()
     double cal_value;
     Tracker penguin_tracker("output.csv");
     Penguin gentoo("Fred", penguin_tracker);
+    Fishing::FishCatcher catcher("fish.csv");
+    Fishing::Fish fish = catcher.catch_fish(Fishing::short_range);
+    std::cout << fish.name << std::endl;
     while (true) {
         std::cout << "How many food items shall I feed you penguin?" << std::endl;
         std::cin >> food_items;
@@ -114,6 +251,6 @@ int main()
             }
         }
     }
-    return 1;
+    return 0;
 }
 
